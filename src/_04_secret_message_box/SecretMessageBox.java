0ecd2f84b6f6ef018edf064b6965c88243a9ad3d
Diaz-Sanchez.Angel <Diaz-Sanchez.Angel@SEMS-259-33.SEMS259.LAB>
2019-11-20 14:56:20 -0800

    hi

--- src/_04_secret_message_box/SecretMessageBox.java
+++ src/_04_secret_message_box/SecretMessageBox.java
@@ -20,14 +20,55 @@ public static void main(String[] args) {
 	// 0. Make a main method and put steps 1-5 inside it
 	
 		// 1. Set a password in a String variable
-		String  = password 
+		String password  = "password"; 
 	
 		// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+String input  = JOptionPane.showInputDialog("GIVE ME A SECRET MESSAGE!???!?!?!?");
 		// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 		//    if they can guess the passcode
-
+String input1 = JOptionPane.showInputDialog("GIVE ME A  SUPER SECRET PASSWORDTO GET IN");
 		// 4. If their guess matches the password, show them the secret message
+if (input1.equals (password))
+{
+
+                             JOptionPane.showMessageDialog(null, input+"");                     
+                              
+                              
+                              
+}  
+                     
+		
+		
+else {
+	
+	JOptionPane.showMessageDialog(null, "intRUDEr");
+	
+	
+}
+		
+		
+		
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 		// 5. If the password does not match, pop-up "INTRUDER!!"
 
