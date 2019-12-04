921a689a5f40d50ab94ec29d96902e81ce5fbb4b
Diaz-Sanchez.Angel <Diaz-Sanchez.Angel@SEMS-259-33.SEMS259.LAB>
2019-12-04 14:56:03 -0800

    robots in space

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -2,24 +2,74 @@ package _07_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
+import javax.tools.Diagnostic;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+ String input = JOptionPane.showInputDialog("What is greater than God,\r\n" + 
+		"more evil than the devil,\r\n" + 
+		"the poor have it,\r\n" + 
+		"the rich need it,\r\n" + 
+		"and if you eat it, you'll die? ");
 
+if (input.equals ("nothing")) {
+	
+	
+	JOptionPane.showMessageDialog(null, "Correct!");
+	
+	score = score+1;
+	
+	
+}
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
 		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
-		
-	}
+else {
+	
+	
+	JOptionPane.showMessageDialog(null, "WRONG. THE ANSWERS NOTHING");
+	
+	
+	
+	
 }
+		// 6. Add some more riddles
+String input1 = JOptionPane.showInputDialog("What has hands but can not clap? ");
+if (input1.equals ("a clock")) {
+	
+	
+	JOptionPane.showMessageDialog(null, "Correct!");
+	
+	score = score+1;
+	
+	
+}
+
+else {
+	
+	
+	
+	JOptionPane.showMessageDialog(null, "WRONG. HOW CAN YOU BE SO BAD AT RIDDLES? THE ANSWERS A CLOCK!");
+	// 2. Make a pop up to show the score.
+			JOptionPane.showMessageDialog(null, "Your score is "+score);
+	
+}
+}
+			
+
+   
+ 
+ 
+
+}
+
+
+
+
 
